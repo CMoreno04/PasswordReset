1fe9307901c9600f87d49db88ec6ca22317857bc
CMoreno04 <cesar.jos.em26@icloud.com>
2021-02-25 21:23:39 -0400

    Create Password reset api

---
+++ PasswordResetAPI/passwordreset-api/src/main/java/com/passwordreset/passwordresetapi/PasswordresetApiApplication.java
@@ -0,0 +1,13 @@
+package com.passwordreset.passwordresetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordresetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordresetApiApplication.class, args);
+	}
+
+}
---
+++ PasswordResetAPI/passwordreset-api/src/test/java/com/passwordreset/passwordresetapi/PasswordresetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordreset.passwordresetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordresetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
